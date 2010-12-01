449b47579775b851fdf5d6e077ad09ac688e27f6
Andreas HUBERT <andreas.hubert@gmx.at>
2010-12-01 16:49:26 +0100

    * added OpenSurf toolbox (pure matlab SURF implementation)

--- matlab/setup.m
+++ matlab/setup.m
@@ -1,4 +1,10 @@
 % setup script to initialise the matlab environment
 
+disp 'Setting up matlab environment for window counting ...'
+
 addpath(pwd);
 addpath(strcat(pwd, '/lib'));
+addpath(strcat(pwd, '/lib/clustering/FUZZCLUST'));
+addpath(strcat(pwd, '/lib/SegTool/SegToolBox/SegToolBox'));
+
+disp '... done'
