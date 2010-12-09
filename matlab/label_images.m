eb5c7c6054be4b5d7a741a2d9d5930d0acbd15f1
Andreas HUBERT <andreas.hubert@gmx.at>
2010-12-09 14:19:05 +0100

    * label_images saves the chosen windows to images/cache

--- matlab/label_images.m
+++ matlab/label_images.m
@@ -12,13 +12,28 @@ files = dir('../images/*.jpg');
 files = [files; dir('../images/*.png')];
 
 fsize = size(files);
+start_imgindex = 0;
+total_imgs = 0;
 for index = 1:fsize(1)
     fname = strcat('../images/', files(index).name);
     matname = strcat(fname, '.mat');
     if exist(matname) == 0
-        info.labels = label_windows(fname);
+        img = imread(fname);
+        info = label_windows(img);
         if do_save
-            save matname info
+            save(matname,'info');
+            num_windows = size(info.x1);
+            size(info.labels)
+            info.x1
+            info.x2
+            info.y1
+            info.y2
+            for c = 1:num_windows(2)
+                w = img(info.x1(1,c):info.x2(1,c), info.y1(1,c):info.y2(1,c), 1:3);
+                ifname = strcat('../images/cache/', num2str(start_imgindex + total_imgs), '.png');
+                imwrite(w, ifname, 'PNG');
+                total_imgs = total_imgs + 1;
+            end
         end
     end
 end
--- matlab/label_windows.m
+++ matlab/label_windows.m
@@ -14,4 +14,8 @@ for index = 1:numpoints
     labels(ROI.x1(1,index):ROI.x2(1,index), ROI.y1(1,index):ROI.y2(1,index)) = 1;
 end
 
-l=labels;
+l.labels=labels;
+l.x1=ROI.x1;
+l.y1=ROI.y1;
+l.x2=ROI.x2;
+l.y2=ROI.y2;
