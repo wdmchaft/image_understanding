7207977e061c3d7b133ffe1076c4fedbe23ae1cc
Andreas HUBERT <andreas.hubert@gmx.at>
2010-12-29 13:26:53 +0100

    * cluster_descriptors has a "labels" parameter now and only takes descriptors where the label is > 0  * info about descriptors, clusters, ... is now stored in data/

--- matlab/cluster_descriptors.m
+++ matlab/cluster_descriptors.m
@@ -1,13 +1,15 @@
-function o=cluster_descriptors(pts, num)
+function o=cluster_descriptors(pts, labels, num_clusters)
 
     data.X=[];
     for i=1:length(pts)
-       data.X(i, :) = pts(i).descriptor';
+       if labels(i) > 0
+           data.X(i, :) = pts(i).descriptor';
+       end
     end
 
     data = clust_normalize(data, 'range');
     
-    param.c = num;
+    param.c = num_clusters;
     param.vis = 0;
     
     o = kmedoid(data, param);
